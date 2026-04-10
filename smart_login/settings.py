11b8dedeb37f0d9a1c21f990fc4c7f548ae24622
banu karthik <priyamurugan.mca13@gmail.com>
2026-04-11 00:03:55 +0530

    fix:csrf

--- smart_login/settings.py
+++ smart_login/settings.py
@@ -105,3 +105,8 @@ CORS_ALLOWED_ORIGINS = config(
     default='http://localhost:3000,http://127.0.0.1:3000'
 ).split(',')
 CORS_ALLOW_CREDENTIALS = True
+# ─── CSRF ─────────────────────────────────────────────────────────────────────
+CSRF_TRUSTED_ORIGINS = config(
+    'CSRF_TRUSTED_ORIGINS',
+    default='http://localhost:3000,http://127.0.0.1:3000'
+).split(',')
